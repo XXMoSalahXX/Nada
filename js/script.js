07d909664f5a52190bfc8bb99c9f075f90c5d636
XXMoSalahXX <96550621+XXMoSalahXX@users.noreply.github.com>
2021-12-22 23:33:06 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ $(function() {
     "use strict";
 
     var typed = new Typed('.type', {
-        strings: [`Happy birthday 🎂, yarab tkon sana agml bkter mn kol ally fat, btmnalk kol altwfe2 fe haytk wtkony as3d bkter fe ally gay, wtkon al5twa ally ht5dyha hya 3wadk 3n ay z3l kan fhyatk, 3ayz a2olk ank agml bnota w2gml o5t wgml had nkd 3lya wnkdt 3keh, frhanlk gdn, rbna ys3dk.`],
+        strings: [`Happy birthday 🎂, yarab tkon sana agml bkter mn kol ally fat, btmnalk kol altwfe2 fe haytk wtkony as3d bkter fe ally gay, wtkon al5twa ally ht5dyha hya 3wadk 3n ay z3l kan fhyatk, 3ayz a2olk ank agml bnota w2gml o5t wgml had nkd 3lya wnkdt 3keh, olt a3mlk haga llzkra wtfdl forever yarb t3gbk, frhanlk gdn, rbna ys3dk.`],
         typeSpeed: 80
     });
 })
